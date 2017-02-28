e49aecbb2a5bb1682f19edf88fea16335a37f41b
Navonil Mukherjee <navonil_mukherjee@yahoo.co.in>
2017-03-01 00:44:36 +0530

    String Builder Test

---
+++ StrArgRev.java
@@ -0,0 +1,12 @@
+/*The following code takes a string as a command line argument, reverses it 
+and prints it.*/
+
+class StrArgRev
+{
+	public static void main(String args[])
+	{
+		int i = 0;
+		StringBuilder str = new StringBuilder(args[0]); // creates StringBuilder object
+		System.out.println(str.reverse()); // calls the reverse() method on the created object
+	}
+}
