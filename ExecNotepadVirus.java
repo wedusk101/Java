c9fd7cf59affcb61ef70734c17d4be450cfb6180
Navonil Mukherjee <navonil_mukherjee@yahoo.co.in>
2017-04-27 13:06:20 +0530

    Update

---
+++ ExecNotepadVirus.java
@@ -0,0 +1,24 @@
+/*The following program mimics a malicious program by invoking the notepad application
+in Windows continuously as long as it runs.*/
+
+import java.io.*;
+
+class ExecNotepadVirus
+{
+	public static void main(String[] args)throws InterruptedException, IOException
+	{
+		while(true)
+		{
+			try{
+				Process p = Runtime.getRuntime().exec("notepad.exe");
+				p.waitFor(); // this line makes sure a new process is spawned only after the previous one is closed
+				Thread.sleep(1); // commenting out or removing the p.waitfor() function call can / will cause the system to hang
+			}
+			catch(Exception e)
+			{
+				e.printStackTrace();
+				System.exit(1);
+			}
+		}
+	}
+}
